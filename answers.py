338b5ac836573bc3644d1cc2fd1805f419cc5a4a
Athiya Deviyani <athiyadee@gmail.com>
2020-03-10 22:01:15 +0000

    Add auto-generated answers.py file

---
+++ answers.py
@@ -0,0 +1,17 @@
+a1a=['.', 'ADJ', 'ADP', 'ADV', 'CONJ', 'DET', 'NOUN', 'NUM', 'PRON', 'PRT', 'VERB', 'X']
+a1b=2649
+a1c=12.05988565013656
+a1d='function'
+a2a=13
+a2b=2.4630453700815003
+a4a3=0.8689827219809665
+a4b1=[('``', '.'), ('My', 'DET'), ('taste', 'NOUN'), ('is', 'VERB'), ('gaudy', 'ADV'), ('.', '.')]
+a4b2=[('``', '.'), ('My', 'DET'), ('taste', 'NOUN'), ('is', 'VERB'), ('gaudy', 'ADJ'), ('.', '.')]
+a4b3='fill me in'
+a4c=56.630575309531835
+a4d=308.7122785474796
+a4e=['DET', 'NOUN', 'ADP', 'DET', 'NOUN', 'VERB', 'ADV']
+a5='fill me in'
+a6='fill me in'
+a3c=16.79319240474419
+a3d='<s>'
