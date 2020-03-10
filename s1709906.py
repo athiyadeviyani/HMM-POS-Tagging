e9ecf8ac1103961c8bd305ddb875032ccb6de6be
Athiya Deviyani <athiyadee@gmail.com>
2020-03-10 15:19:20 +0000

    Updated with appropriate comments

--- s1709906.py
+++ s1709906.py
@@ -1,5 +1,4 @@
 import inspect, sys, hashlib
-import nltk, inspect, math, numpy as np
 
 # Hack around a warning message deep inside scikit learn, loaded by nltk :-(
 #  Modelled on https://stackoverflow.com/a/25067818
@@ -21,10 +20,9 @@ from nltk.tag import map_tag, tagset_mapping
 
 # MY IMPORTS
 from nltk.tag.hmm import HiddenMarkovModelTagger
-from nltk.probability import ConditionalFreqDist
-from nltk.probability import ConditionalProbDist, LidstoneProbDist
+from nltk.probability import ConditionalFreqDist, ConditionalProbDist, LidstoneProbDist
 from math import log
-
+import numpy as np
 
 if map_tag('brown', 'universal', 'NR-TL') != 'NOUN':
     # Out-of-date tagset, we add a few that we need
@@ -68,19 +66,16 @@ class HMM:
         :return: The emission probability distribution and a list of the states
         :rtype: Tuple[ConditionalProbDist, list(str)]
         """
-        # raise NotImplementedError('HMM.emission_model')
-        # TODO prepare data
+        # Prepare the data
 
         # Don't forget to lowercase the observation otherwise it mismatches the test data
         # Do NOT add <s> or </s> to the input sentences
-        # [(tag, word.lower()) for i in a for (word, tag) in i]
-        # data = [(tag, word.lower()) for (word, tag) in train_data]
-        data = [(tag, word.lower()) for pairs in train_data for (word, tag) in pairs] 
+        data = [(tag, word.lower()) for pairs in train_data for (word, tag) in pairs]
 
-        # TODO compute the emission model
+        # Compute the emission model
         emission_FD = ConditionalFreqDist(data)
         self.emission_PD = ConditionalProbDist(emission_FD, LidstoneProbDist, 0.01)
-
+        
         for tag, word in data:
             if tag not in self.states:
                 self.states.append(tag)
@@ -100,10 +95,7 @@ class HMM:
         :return: log base 2 of the estimated emission probability
         :rtype: float
         """
-        # raise NotImplementedError('HMM.elprob')
-        test1 = log(self.emission_PD['NOUN'].prob('fulton')) # -7.47644570515
-        test2 = log(self.emission_PD['X'].prob('fulton')) # -8.54286093816
-        return test1, test2
+        return log(self.emission_PD['VERB'].prob('is')) # -1.4
 
     # Compute transition model using ConditionalProbDist with a LidstonelprobDist estimator.
     # See comments for emission_model above for details on the estimator.
@@ -116,25 +108,23 @@ class HMM:
         :return: The transition probability distribution
         :rtype: ConditionalProbDist
         """
-        # raise NotImplementedError('HMM.transition_model')
-        # TODO: prepare the data
+        # Prepare the data
+        data = []
+        tags = []
 
         # The data object should be an array of tuples of conditions and observations,
         # in our case the tuples will be of the form (tag_(i),tag_(i+1)).
         # DON'T FORGET TO ADD THE START SYMBOL </s> and the END SYMBOL </s>
-        tags = []
+        for s in train_data:
+            start = ["<s>"]
+            start.extend([tag for (word, tag) in s])
+            start.extend(["</s"])
+            tags.extend(start)
 
-        # add start symbol and end symbol
-        for tagged_sentence in train_data:
-            i = ["<s>"]
-            i.extend([tag for (word, tag) in tagged_sentence])
-            i.extend(["</s>"])
-            tags.extend(i)
+        for i in range(len(tags) - 1):
+            data.append((tags[i], tags[i+1]))
 
-        data = [(tags[i], tags[i+1]) for i in range(len(tags) - 1)]
-
-        # TODO compute the transition model
-
+        # Compute the transition model
         transition_FD = ConditionalFreqDist(data)
         lidstone_estimator = lambda fd: LidstoneProbDist(fd, 0.01, fd.B() + 1)
         self.transition_PD = ConditionalProbDist(transition_FD, lidstone_estimator)
@@ -154,9 +144,8 @@ class HMM:
         :return: log base 2 of the estimated transition probability
         :rtype: float
         """
-        # raise NotImplementedError('HMM.tlprob')
         transition_PD = self.transition_model(self.train_data)
-        return -transition_PD[state1].logprob(state2) # fixme
+        return -transition_PD[state1].logprob(state2) # negative log probability
 
     # Train the HMM
     def train(self):
@@ -170,7 +159,6 @@ class HMM:
 
     # Initialise data structures for tagging a new sentence.
     # Describe the data structures with comments.
-    # TODO: CHANGE THE COMMENTS!
     # Use the models stored in the variables: self.emission_PD and self.transition_PD
     # Input: first word in the sentence to tag
     def initialise(self, observation):
@@ -180,37 +168,52 @@ class HMM:
         :param observation: the first word in the sentence to tag
         :type observation: str
         """
-        # raise NotImplementedError('HMM.initialise')
         # Initialise step 0 of viterbi, including
         #  transition from <s> to observation
         # use costs (-log-base-2 probabilities)
-
-        # The Viterbi data structure contains the Viterbi path probabilities as a T by N
-        # table where T is the number of observations and N is the number of states or tags.
-        # Each cell contains the most probable path by taking the maximum over all possible
-        # previous state sequences to arrive at that state.
-        self.viterbi = []
+        
+        # The Viterbi data structure is a T by N table
+        #   where T = number of observations
+        #         N = number of states/tags
+        # Each cell of the table contains the most probable path 
+        # i.e. the maximum of all possible previous state sequences to arrive at that state
+        # self.viterbi = [] is already defined in __init__()
         self.viterbi.append([])
 
         # Initialise step 0 of backpointer
 
-        # The backpointer data structure keeps track of the best path of hidden states that
-        # led to each state in a T by N table where T is the number of observations and N is
-        # the number of states. Each cell contains the state which had the maximum viterbi
-        # probability (from the Viterbi table) in the pervious time step (or observation).
-        self.backpointer = []
+        # The backpointer data structure keeps track of the indexes which point to states with
+        #   the highest viterbi probability (from the viterbi table)
+        # This will used to backtrack from the last state to determine the final maximum path back 
+        #   to the starting point
+        # The backpointer data structure is a T by N table
+        #   where T = number of observations
+        #         N = number of states
+        # Each cell of the table contains the state that has the maximum viterbi probability
+        # in the previous observation
+        # self.backpointer = [] is already defined in __init__()
         self.backpointer.append([])
 
-        # At intialise, cost with +logprob or *prob
+        # Go through the states
+        # Calculate the probability of being at that state
+        #   where probability = previous viterbi probability (at initialisation, this is none) + 
+        #       probabiity to transition from previous state (at initialisation, the previous state is <s>) +
+        #       emission probability of current state
         for state in self.states:
-            # Probability of transition from state '<s>' to state 'state'
-            transition = self.transition_PD["<s>"].logprob(state)
+            
+            # Transition probability is the probability to transition from state '<s>' to state 'state'
+            transition_prob = -self.transition_PD['<s>'].logprob(state)
 
-            # Probability of observation 'observation' given the current state 'state'
-            emission = self.emission_PD[state].logprob(observation)
+            # Emission probability is the probability of observation 'observation' to current state 'state'
+            emission_prob = -self.emission_PD[state].logprob(observation)
 
-            self.viterbi[0][state] = transition + emission
-            self.backpointer[state] = [state]
+            # Current probability = prev + transition_prob + emission_prob
+            #       in this case, prev = 0
+            current_state_probability = transition_prob + emission_prob
+            self.viterbi.append([current_state_probability])
+            
+            # Backpointer initially points to the the start state <s> with index 0 (self.states[0])
+            self.backpointer.append([0])
 
     # Tag a new sentence using the trained model and already initialised data structures.
     # Use the models stored in the variables: self.emission_PD and self.transition_PD.
@@ -224,77 +227,79 @@ class HMM:
         :type observations: list(str)
         :return: List of tags corresponding to each word of the input
         """
-        # raise NotImplementedError('HMM.tag')
         tags = []
 
         for t in range(1, len(observations)): # iterate over steps
             for s in range(len(self.states)): # iterate over states
+                # Update the viterbi and backpointer data structures
+                # Use costs, not probabilities
+                state_probs = []
 
-                # list of probabilities and pointers to previous states
-                # and take the most probable
-                state_probabilities = []
+                # Go through all previous probabilities
+                #   Calculate the probability of being in that state
+                #       where probability = maximum of previous viterbi probabilities * 
+                #               probability to transition from that previous state to the current state
+                for prev_state in range(len(self.states)):
 
-                # go through all previous probabilities
-                #   calculate the probability of being in that state
-                #   probability = max of previous viterbi probabilities * 
-                #       probability to transition from that previous state to current state
-                for prevs in range(len(self.states)):
+                    # Previous probability is the viterbi for the previous observation (at time step t-1)
+                    previous_prob = self.viterbi[prev_state][t-1]
 
-                    # previous prob is viterbu probability for the previous observation (t-1)
-                    previous_prob = self.viterbi[prevs][t-1] # prev_prob is already neg log
+                    # Transition probability is the probability of transitioning from state prev_state to state s
+                    transition_prob = -self.transition_PD[self.states[prev_state]].logprob(self.states[s])
 
-                    # emission prob is probability of observation i given current state i
-                    emission_prob = log(self.emission_PD[self.states[s]].prob(observations[t]), 2)
+                    # Emission probability is the probability of observation t given current state s
+                    emission_prob = -self.emission_PD[self.states[s]].logprob(observations[t])
 
-                    # transition prob is probability of transition from state prevs to state i
-                    transition_prob = log(self.transition_PD[self.states[prevs]].prob(self.states[s]),2)
+                    # Current probability = previous_prob + emission_prob + transition_prob
+                    #   We are summing them up because we have used the negative log probabilities
+                    current_prob = previous_prob + emission_prob + transition_prob
+                    state_probs += [current_prob]
 
-                    # current probability is the sum of all three above, negative log probabilities are
-                    # used so we sum up instead of multiplying
-                    probability     = previous_prob - emission_prob - transition_prob
-                    state_probs     = state_probs + [probability]
+                # Add the maximum probability of all calculated probabilities to the respective path
+                # for state s in the Viterbi table
+                # Since we have used negative log probabilities, find the min
+                max_prob = min(state_probs)
+                self.viterbi[s].append(max_prob)
 
+                # Add the index of the state which produced the maximum probability of all calculated
+                # probabilities to the backpointer table for state s
+                state_max_index = np.argmin(state_probs)
+                self.backpointer[s].append(state_max_index)
 
-                # add the max prob of calculated probabilities to the corresponding path in viterbi list
-                self.viterbi[s].append(min(state_probs))
-
-                # add the backpointer to the state which produced the
-                # max prob of calculated probabilities
-                self.backpointer[s].append(np.argmin(state_probs))
-
-
-        
         # Add a termination step with cost based solely on cost of transition to </s> , end of sentence.
-        end_probs = []
+        termination_probs = []
 
-        # Similar to above, go through all states and add the probability for them to finish, that is, 
-        # to transit from the state to end state </s>
-        for i in range(len(self.states)):
-            previous_prob = self.viterbi[i][len(observations) - 1]
-            prob_of_end = log(self.transition_model[self.states[i]].prob("</s>"),2)
-            total_prob = previous_prob - prob_of_end # negative log probability
-            end_probs = end_probs + [total_prob]
+        # Go through all the states
+        #   Add the transition probability from that state to the end state </s>
+        for s in range(len(self.states)):
+            prev_prob = self.viterbi[s][len(observations) - 1]
+            transition_prob = -self.transition_PD[self.states[s]].logprob('</s>')
+            current_prob = prev_prob + transition_prob
+            termination_probs += [current_prob]
 
         # Reconstruct the tag sequence using the backpointer list.
         # Return the tag sequence corresponding to the best path as a list.
         # The order should match that of the words in the sentence.
 
-        # The first backpointer points to the state which had the largest probability
-        # at the end (previous viterbi probability + probability to finish)
-        last_bp = np.argmin(end_probs)
+        # First backpointer points to the index of the state which has the largest
+        # probability in termination_probs
+        #   i.e. maximum prev_prob + transition_prob 
+        #           where transition_prob is the transition to </s> from current state
+        last = np.argmin(termination_probs)
 
-        # Append the last state to the tags list
-        tags.append(self.states[last_bp])
+        # Append the corresponding state to the list of tags
+        tags.append(self.states[last])
 
-        # Go through the observation indexes from end to start
-        # Add the backpointed state to the tags list
-        # Update the backpointer to the one which came from the previously backpointed state
-        for i in range(len(observations)-1, 0, -1):
-            bp = self.backpointer[last_bp][i]
-            tags.append(self.states[bp])
-            last_bp = bp
+        # Iterate through the observation indexes from end to start
+        #   Append the corresponding backpointed state to the list of tags
+        #   Update the backpointer to point to the previous backpointed state
+        for t in range(len(observations)-1, 0, -1):
+            backpointer = self.backpointer[last][t]
+            tags.append(self.states[backpointer])
+            last = backpointer
 
-        # Reverse tags to match the order of observations
+        # We want to return the tags in the same order of the observations
+        # Therefore, we have to reverse it
         tags.reverse()
 
         return tags
@@ -308,13 +313,13 @@ class HMM:
 
         :param state: A tag name
         :type state: str
-        :param step: The (0-origin) number of a step
+        :param step: The (0-origin) number of a step:  if negative,
+          counting backwards from the end, i.e. -1 means the last step
         :type step: int
         :return: The value (a cost) for state as of step
         :rtype: float
         """
-        raise NotImplementedError('HMM.get_viterbi_value')
-        return ... # fix me
+        return self.viterbi[state][step]
 
     # Access function for testing the backpointer data structure
     # For example model.get_backpointer_value('VERB',2) might be 'NOUN'
@@ -325,13 +330,13 @@ class HMM:
 
         :param state: A tag name
         :type state: str
-        :param step: The (0-origin) number of a step
-        :type step: str
+        :param step: The (0-origin) number of a step:  if negative,
+          counting backwards from the end, i.e. -1 means the last step
+        :type step: int
         :return: The state name to go back to at step-1
         :rtype: str
         """
-        raise NotImplementedError('HMM.get_backpointer_value')
-        return ... # fix me
+        return self.backpointer[state][step]
 
 def answer_question4b():
     """
@@ -444,7 +449,7 @@ def answers():
            print('viterbi value (%s) must be a cost'%v_sample,file=sys.stderr)
 
     b_sample=model.get_backpointer_value('VERB',5)
-    if not (type(b_sample)=='str' and b_sample in model.steps):
+    if not (type(b_sample)==str and b_sample in model.states):
            print('backpointer value (%s) must be a state name'%b_sample,file=sys.stderr)
 
 
